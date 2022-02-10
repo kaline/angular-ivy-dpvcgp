020e3515c57c265020576530898456af422a0960
kaline <mesquitabfkaline@gmail.com>
2022-02-10 11:05:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-dpvcgp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-dpvcgp)
---
+++ src/app/kaline.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kaline',
+  template: `<h1>Kaline</h1>`,
+  styles: [`h1{ color: pink; }`],
+})
+export class kalineComponent {}
